c50d3ef185d42c8d95113a41d1f24a0d6f711bde
Oliwier Piasecki <piaseckioliwier@gmail.com>
2023-06-12 23:16:41 +0200

    Update forest.py

--- model/3h/RandomForestRegressor/forest.py
+++ model/3h/RandomForestRegressor/forest.py
@@ -4,22 +4,29 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from matplotlib.widgets import CheckButtons
 
-def read_data(file_path):
-    column_names = ["name", "lat", "lon", "temp", "humidity", "pressure", "wind_speed", "wind_deg", "timestamp",
-                    "pm2_5", "pm10", "co", "no", "no2", "o3", "so2", "nh3", "temp_label", "humidity_label",
-                    "pressure_label", "wind_speed_label", "wind_deg_label", "pm2_5_label", "pm10_label", "co_label",
-                    "no_label", "no2_label", "o3_label", "so2_label", "nh3_label"]
-    data = pd.read_csv(file_path, header=None, names=column_names)
-    data['timestamp'] = pd.to_datetime(data['timestamp'])
-    return data
+class LasLosowy:
+    def __init__(self):
+        self.data_train = None
+        self.data_pred = None
+        self.data_train_path = None
+        self.data_pred_path = None
 
-def select_features(data, selected_features):
-    return data[selected_features]
+    def read_data(self, file_path):
+        column_names = ["name", "lat", "lon", "temp", "humidity", "pressure", "wind_speed", "wind_deg", "timestamp",
+                        "pm2_5", "pm10", "co", "no", "no2", "o3", "so2", "nh3", "temp_label", "humidity_label",
+                        "pressure_label", "wind_speed_label", "wind_deg_label", "pm2_5_label", "pm10_label", "co_label",
+                        "no_label", "no2_label", "o3_label", "so2_label", "nh3_label"]
+        data = pd.read_csv(file_path, header=None, names=column_names)
+        data['timestamp'] = pd.to_datetime(data['timestamp'])
+        return data
 
-def train_model(X_train, y_train, params):
-    model = RandomForestRegressor(**params)
-    model.fit(X_train, y_train)
-    return model
+    def select_features(self, data, selected_features):
+        return data[selected_features]
 
-def predict(model, X_pred):
-    return model.predict(X_pred)
+    def train_model(self, X_train, y_train, params):
+        model = RandomForestRegressor(**params)
+        model.fit(X_train, y_train)
+        return model
+
+    def predict(self, model, X_pred):
+        return model.predict(X_pred)
