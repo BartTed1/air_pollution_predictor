10f8bd9e02e1c22be1de3e7b00b23840ef95a439
IamKrytas <96568740+IamKrytas@users.noreply.github.com>
2023-06-13 13:17:05 +0200

    Refactor statistics.py

--- statistics/3h/statistics.py
+++ statistics/3h/statistics.py
@@ -1,39 +1,21 @@
 import numpy as np
 import statsmodels.api as sm
-import datetime
 import csv
-
-
-def timestamp_to_day_in_week_number(timestamp) -> int:
-	date = datetime.datetime.fromtimestamp(int(timestamp))
-	return int(date.weekday())
-
-
-def timestamp_to_month_number(timestamp) -> int:
-	date = datetime.datetime.fromtimestamp(int(timestamp))
-	return int(date.month)
-
-
-def timestamp_to_hour(timestamp) -> int:
-	date = datetime.datetime.fromtimestamp(int(timestamp))
-	return int(date.hour)
-
+import timeutils
 
 # use data_3h.csv
 # Load data from CSV
-
 with open('../../data/3h/data_3h_Bydgoszcz.csv', 'r') as file:
 	csv_reader = csv.reader(file)
 	data = []
 	labels = []
 	for row in csv_reader:
 		data.append([
-			timestamp_to_day_in_week_number(row[8]),
-			timestamp_to_hour(row[8]),
+			timeutils.timestamp_to_day_in_week_number(row[8]),
+			timeutils.timestamp_to_hour(row[8]),
 			(1 / float(row[17])) * 10 * (float(row[17]) - float(row[3])),  # temp_change importance
 			float(row[6]),  # wind_speed
 			float(row[9]),  # pm2_5
-
 		])
 		labels.append(float(row[22]))  # pm2_5_label
 
