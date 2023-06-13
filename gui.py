bf2bfd5fbfc4b8f3c4e6e24eff338621a5b81462
IamKrytas <96568740+IamKrytas@users.noreply.github.com>
2023-06-13 13:11:47 +0200

    Refactor gui.py

--- gui.py
+++ gui.py
@@ -1,6 +1,5 @@
 import csv
 import sys
-import numpy as np
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QLineEdit, QMessageBox, QLabel, QLabel
 from PyQt5.QtCore import Qt, QFileInfo
 from PyQt5.QtGui import QIcon, QFont
@@ -9,35 +8,33 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 
 sys.path.append('./model/3h/RandomForestRegressor')
-from forest import LasLosowy
-forest = LasLosowy()
+from forest import RandomForestRegressorModel
+forest = RandomForestRegressorModel()
 
 sys.path.append('./model/3h/NeuralNetwork')
 import nntest as nnTest
 import neural_network as nn
 import timeutils as ts
 
-#TODO dodac okno z marginesem bledow
-
 class GUI(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Przewidywanie pogody")
-        self.setFixedSize(400, 250)  # Ustawienie stałego rozmiaru okna
+        self.setWindowTitle("Pollution prediction")
+        self.setFixedSize(400, 250)  # Set fixed size
 
-        # ikonka programu
+        # program icon
         logo_icon = QIcon("logo.png")
         self.setWindowIcon(logo_icon)
     
-        # napis glowny 
-        self.start = QLabel("Predykcja wartości PM2,5", self)
+        # main label
+        self.start = QLabel("PM 2.5 value prediction", self)
         self.start.setGeometry(0, 10, 400, 30)
         font = QFont("Arial", 12, QFont.Bold)
         self.start.setFont(font)
         self.start.setAlignment(Qt.AlignCenter)
 
-        # wczytanie 1 pliku
-        self.tekst1plik = QLabel('Wczytaj plik csv z danymi do nauki:', self)
+        # load first file
+        self.tekst1plik = QLabel("Load a csv file with study data: ", self)
         self.tekst1plik.setGeometry(20, 50, 400, 30)
 
         self.line_edit = QLineEdit(self)
@@ -45,12 +42,12 @@ class GUI(QMainWindow):
         self.line_edit.setReadOnly(True)
         self.line_edit.setStyleSheet("border: 0.5px solid black;")
 
-        self.button = QPushButton("Wybierz plik", self)
+        self.button = QPushButton("Choose a file", self)
         self.button.setGeometry(190, 75, 100, 30)
         self.button.clicked.connect(lambda: self.select_file('train'))
 
-        # wczytanie 2 pliku
-        self.tekst2plik = QLabel('Wczytaj plik csv z danymi do predykcji:', self)
+        # load second file
+        self.tekst2plik = QLabel("Load a csv file with prediction data: ", self)
         self.tekst2plik.setGeometry(20, 110, 400, 30)
 
         self.line_edit2 = QLineEdit(self)
@@ -58,46 +55,46 @@ class GUI(QMainWindow):
         self.line_edit2.setReadOnly(True)
         self.line_edit2.setStyleSheet("border: 0.5px solid black;")
 
-        self.button2 = QPushButton("Wybierz plik", self)
+        self.button2 = QPushButton("Choose a file", self)
         self.button2.setGeometry(190, 135, 100, 30)
         self.button2.clicked.connect(lambda: self.select_file('pred'))
 
-        # generowanie wykresow
-        self.button_wykres = QPushButton("Generuj wykres", self)
+        # Generate chart
+        self.button_wykres = QPushButton("Generate chart", self)
         self.button_wykres.setGeometry(120, 190, 150, 30)
         self.button_wykres.clicked.connect(self.generate_plot)
 
     def select_file(self, data_type):
         options = QFileDialog.Options()
-        file_name, _ = QFileDialog.getOpenFileName(self, "Wybierz plik", "", "Wszystkie pliki (*)", options=options)
+        file_name, _ = QFileDialog.getOpenFileName(self, "Choose a file", "", "All files (*)", options=options)
         if file_name:
             if file_name.lower().endswith('.csv'):
                 file_info = QFileInfo(file_name)
                 if data_type == 'train':
                     self.line_edit.setText(file_info.fileName())
                     self.data_train = forest.read_data(file_name)
-                    self.data_train_path = file_name  # Ścieżka do pliku
+                    self.data_train_path = file_name  # path to file
                 elif data_type == 'pred':
                     self.line_edit2.setText(file_info.fileName())
                     self.data_pred = forest.read_data(file_name)
-                    self.data_pred_path = file_name  # Ścieżka do pliku
+                    self.data_pred_path = file_name  # path to file
             else:
-                QMessageBox.critical(self, "Błąd", "Wybrano nieprawidłowy plik. Proszę wybrać plik CSV.")
+                QMessageBox.critical(self, "ERROR!", "Invalid file selected. Please select a CSV file.")
 
     def generate_plot(self):
         if self.line_edit.text() == '' or self.line_edit2.text() == '':
-            QMessageBox.warning(self, "Błąd", "Nie wybrano obu plików. Proszę wybrać pliki CSV.")
+            QMessageBox.warning(self, "ERROR!", "Both files are not selected. Please select CSV files.")
             return
 
-        # Wybór wybranych cech
+        # Selection of selected features
         selected_features = ['pm2_5', 'wind_speed', 'wind_deg', 'temp', 'humidity']
 
-        # Podział danych na cechy i wartości oczekiwane
+        # Division of data into features and expected values
         X_train = forest.select_features(self.data_train, selected_features)
         y_train = self.data_train['pm2_5_label']
         X_pred = forest.select_features(self.data_pred, selected_features)
 
-        # Parametry modelu
+        # Model parameters
         params = {
             'criterion': 'absolute_error',
             'max_depth': 15,
@@ -108,19 +105,19 @@ class GUI(QMainWindow):
             'n_estimators': 150
         }
 
-        # Tworzenie nowej instancji obiektu Figure i FigureCanvas
+        # Create a new instance of Figure and FigureCanvas
         self.figure = Figure()
         self.canvas = FigureCanvas(self.figure)
         self.canvas.setGeometry(10, 50, 780, 540)
         self.ax = self.figure.add_subplot(111)
 
-        # Trening modelu
+        # Model training
         model = forest.train_model(X_train, y_train, params)
 
-        # Przewidywanie wartości
+        # Value prediction
         predictions = forest.predict(model, X_pred)
 
-        # SIECI NEURONOWE
+        # Neural network
         with open(self.data_train_path, 'r') as file:
             csv_reader = csv.reader(file)
             data = []
@@ -153,38 +150,26 @@ class GUI(QMainWindow):
         # Create a neural network object
         neural_network = nn.NeuralNetwork(data, labels)
 
-        # Tutaj dodac bar loading
+        #TODO: Add loading bar
+
         # Build and train the neural network
         neural_network.build_and_train()
         
         # Create a test object
         test = nnTest.NNTest(neural_network, data2, labels2)
 
-        #print(data)
-
         # Print predictions
         predictions_neural = test.forecast(12)
-        #print(predictions)
-
-        # Save the neural network
-        #neural_network.save()
-
-        # Load the neural network
-        #neural_network.load('model.h5')
-
-        # Use the neural network to make predictions
-        # neural_network.predict()
 
         # Convert predictions to a list of pm2.5 values
         pm25_predictions = [pred[0] for pred in predictions_neural]
-        # KONIEC SIECI NEURONOWYCH
+        # Neural network END
 
-        # Generowanie wykresu
+        # Chart generation
         fig, ax = plt.subplots(figsize=(8, 6))
-
-        line1, = ax.plot(self.data_pred['pm2_5'], label='Wartości prawdziwe')
-        line2, = ax.plot(predictions, label='Las losowy')
-        line3, = ax.plot(pm25_predictions, label='Sieci neuronowe')
+        line1, = ax.plot(self.data_pred['pm2_5'], label='Real values')
+        line2, = ax.plot(predictions, label='Random forest')
+        line3, = ax.plot(pm25_predictions, label='Neutal network')
         lines = [line1, line2, line3]
 
         prawdziwe_wartosci_pm2_5 = self.data_pred['pm2_5']
@@ -217,92 +202,88 @@ class GUI(QMainWindow):
             fig.canvas.draw()
 
         fig.canvas.mpl_connect('pick_event', on_pick)
-        plt.xlabel('Indeks próbki')
-        plt.ylabel('Wartość pm2_5')
-        plt.title('Porównanie wartości aktualnych i przewidywanych')
-
+        plt.xlabel('Sample index')
+        plt.ylabel('pm2_5 value')
+        plt.title('Comparison of current and predicted values')
         plt.show()
 
         correct, incorrect = test.pobierz_trendy()
         text = f"Correct: {correct}, Incorrect: {incorrect}"
-
         text2 = f"Correct: {correct_forest}, Incorrect: {incorrect_forest}"
 
-        def oblicz_margines_bledu(wartosci_rzeczywiste, przewidywania):
-            if len(wartosci_rzeczywiste) != len(przewidywania):
+        def calculate_error_margin(real_values, predicted_values):
+            if len(real_values) != len(predicted_values):
                 raise ValueError("Liczba wartości rzeczywistych i przewidywanych musi być taka sama.")
             
-            liczba_wartosci = len(wartosci_rzeczywiste)
-            margines_bledu = sum(abs(przewidywania[i] - wartosci_rzeczywiste[i]) / wartosci_rzeczywiste[i] for i in range(liczba_wartosci))
-            procentowy_margines_bledu = (margines_bledu / liczba_wartosci) * 100
-            
-            tekst_procentowy_margines_bledu = "{:.2f}%".format(procentowy_margines_bledu)
+            number_values = len(real_values)
+            margin_of_error = sum(abs(predicted_values[i] - real_values[i]) / real_values[i] for i in range(number_values))
+            percentage_margin_of_error = (margin_of_error / number_values) * 100
+            tekst_percentage_margin_of_error = "{:.2f}%".format(percentage_margin_of_error)
+            return tekst_percentage_margin_of_error
 
-            return tekst_procentowy_margines_bledu
+        # Calculating the margin of error
+        margin_of_error_sieci_neuronowe = calculate_error_margin(prawdziwe_wartosci_pm2_5, pm25_predictions)
+        margin_of_error_las_losowy = calculate_error_margin(prawdziwe_wartosci_pm2_5, predictions)
 
-        # obliczanie marginesu bledu
-        margines_bledu_sieci_neuronowe = oblicz_margines_bledu(prawdziwe_wartosci_pm2_5, pm25_predictions)
-        margines_bledu_las_losowy = oblicz_margines_bledu(prawdziwe_wartosci_pm2_5, predictions)
-
-        # OKNO TRENDOW
-        self.oknoTrendow = QMainWindow()
-        self.oknoTrendow.setWindowTitle("Trendy")
-        self.oknoTrendow.setFixedSize(400, 300)
-        self.oknoTrendow.move(100, 100)
+        # Trend Window
+        self.TrendWindow = QMainWindow()
+        self.TrendWindow.setWindowTitle("Trends")
+        self.TrendWindow.setFixedSize(400, 300)
+        self.TrendWindow.move(100, 100)
         logo_icon = QIcon("logo.png")
-        self.oknoTrendow.setWindowIcon(logo_icon)
+        self.TrendWindow.setWindowIcon(logo_icon)
 
         font = QFont("Arial", 12, QFont.Bold)
 
-        labelGlownyTrendy = QLabel("TRENDY", self.oknoTrendow)
+        labelGlownyTrendy = QLabel("TRENDS", self.TrendWindow)
         labelGlownyTrendy.setFont(font)
         labelGlownyTrendy.setGeometry(0, -60, 400, 300)
         labelGlownyTrendy.setAlignment(Qt.AlignCenter)
 
-        label = QLabel("Sieci neuronowe", self.oknoTrendow)
+        label = QLabel("Neural Network", self.TrendWindow)
         label.setGeometry(0, -30, 400, 300)
         label.setAlignment(Qt.AlignCenter)
 
-        label2 = QLabel(text, self.oknoTrendow)
+        label2 = QLabel(text, self.TrendWindow)
         label2.setGeometry(0, 0, 400, 300)
         label2.setAlignment(Qt.AlignCenter)
 
-        label3 = QLabel("Las losowy", self.oknoTrendow)
+        label3 = QLabel("Random forest", self.TrendWindow)
         label3.setGeometry(0, 30, 400, 300)
         label3.setAlignment(Qt.AlignCenter)
 
-        label4 = QLabel(text2, self.oknoTrendow)
+        label4 = QLabel(text2, self.TrendWindow)
         label4.setGeometry(0, 60, 400, 300)
         label4.setAlignment(Qt.AlignCenter)
 
-        self.oknoTrendow.show()
+        self.TrendWindow.show()
 
-        # OKNO MARGINESU BLEDU
+        # ERROR MARGIN WINDOW
         self.oknoMarginesuBledu = QMainWindow()
-        self.oknoMarginesuBledu.setWindowTitle("Marginesy błędu")
+        self.oknoMarginesuBledu.setWindowTitle("Margin of error")
         self.oknoMarginesuBledu.setFixedSize(400, 300)
         self.oknoMarginesuBledu.move(100, 500)
         logo_icon = QIcon("logo.png")
         self.oknoMarginesuBledu.setWindowIcon(logo_icon)
 
-        labelGlownyMarginesBledu = QLabel("MARGINESY BŁĘDU", self.oknoMarginesuBledu)
+        labelGlownyMarginesBledu = QLabel("MARGIN OF ERROR", self.oknoMarginesuBledu)
         labelGlownyMarginesBledu.setFont(font)
         labelGlownyMarginesBledu.setGeometry(0, -60, 400, 300)
         labelGlownyMarginesBledu.setAlignment(Qt.AlignCenter)
 
-        label5 = QLabel("Sieci neuronowe", self.oknoMarginesuBledu)
+        label5 = QLabel("Neural Network", self.oknoMarginesuBledu)
         label5.setGeometry(0, -30, 400, 300)
         label5.setAlignment(Qt.AlignCenter)
 
-        label6 = QLabel(margines_bledu_sieci_neuronowe, self.oknoMarginesuBledu)
+        label6 = QLabel(margin_of_error_sieci_neuronowe, self.oknoMarginesuBledu)
         label6.setGeometry(0, 0, 400, 300)
         label6.setAlignment(Qt.AlignCenter)
 
-        label7 = QLabel("Las losowy", self.oknoMarginesuBledu)
+        label7 = QLabel("Random Forest", self.oknoMarginesuBledu)
         label7.setGeometry(0, 30, 400, 300)
         label7.setAlignment(Qt.AlignCenter)
 
-        label8 = QLabel(margines_bledu_las_losowy, self.oknoMarginesuBledu)
+        label8 = QLabel(margin_of_error_las_losowy, self.oknoMarginesuBledu)
         label8.setGeometry(0, 60, 400, 300)
         label8.setAlignment(Qt.AlignCenter)
 
